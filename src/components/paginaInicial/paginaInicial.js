bef7d32f103124d75c9fbc7c8b4c331e30a58e65
Andresa Alves <andresa_15ga@hotmail.com>
2023-01-23 21:55:51 -0300

    Iniciando projeto

---
+++ src/components/paginaInicial/paginaInicial.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+export function paginaInicial() {
+    return (
+        <main>
+            <div>
+                <h1>Preencha os dados abaixo e tenha o resultado do seu IMC</h1>
+            </div>
+
+            <div></div>
+        </main>
+    )
+}
